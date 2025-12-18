40a1fd64eb3b3ed0939cd36ffcf1597745057179
bala-amypo <bala@amypo.in>
2025-12-18 08:46:38 +0000

    User commit

--- src/main/java/com/example/demo/controller/CrimeReportController.java
+++ src/main/java/com/example/demo/controller/CrimeReportController.java
@@ -1,9 +1,9 @@
 package com.example.demo.controller;
 
-// /* // import com.example.demo.entity.CrimeReport;
-// // import com.example.demo.service.CrimeReportService;
-// // import org.springframework.http.ResponseEntity;
-// // import org.springframework.web.bind.annotation.*; 
+import com.example.demo.entity.CrimeReport;
+import com.example.demo.service.CrimeReportService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*; 
 
 public class CrimeReportController{
 
