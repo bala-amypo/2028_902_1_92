4f8122c85b833ccb6a33e562f9c50b446f8ceb26
bala-amypo <bala@amypo.in>
2025-12-18 08:46:14 +0000

    User commit

--- src/main/java/com/example/demo/controller/HotspotZoneController.java
+++ src/main/java/com/example/demo/controller/HotspotZoneController.java
@@ -1,5 +1,6 @@
 package com.example.demo.controller;
-
+import org.spring.framework.web.bind.annotation.RestController;
+import org.spring.framework.web.bind.annotation.RequestMapping;
 public class HotspotZoneController{
 
 }
