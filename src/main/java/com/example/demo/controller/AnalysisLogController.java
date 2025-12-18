afa077f461df54f0c18cbe4c783e108e43501441
bala-amypo <bala@amypo.in>
2025-12-18 08:58:32 +0000

    User commit

--- src/main/java/com/example/demo/controller/AnalysisLogController.java
+++ src/main/java/com/example/demo/controller/AnalysisLogController.java
@@ -1,6 +1,8 @@
 package com.example.demo.controller;
 import com.example.demo.entity.AnalysisLog;
-import com.example.demo.service.
+import com.example.demo.service.AnalysisLogService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 public class AnalysisLogController{
 
 }
