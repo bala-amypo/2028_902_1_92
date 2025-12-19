d548526da8c25752bff02466cbdbd39cf0558c87
bala-amypo <bala@amypo.in>
2025-12-19 16:04:05 +0000

    User commit

--- src/main/java/com/example/demo/service/HotspotZoneService.java
+++ src/main/java/com/example/demo/service/HotspotZoneService.java
@@ -1,13 +1,13 @@
 
 package com.example.demo.repository;
 
-import com.example.demo.model.demoZone;
+import com.example.demo.model.HotspotZone;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 import java.util.Optional;
 
-public interface HotspotZoneRepository extends JpaRepository<demoZone, Long> {
+public interface HotspotZoneRepository extends JpaRepository<HotspotZone, Long> {
 
 List<HotspotZone> findBySeverityLevel(String level);
 
